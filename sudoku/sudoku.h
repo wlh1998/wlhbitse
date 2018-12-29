e9666b980ce96dc619e4436fa54f837375d7d41a
wlh1998 <34409620+wlh1998@users.noreply.github.com>
2018-12-29 22:47:03 +0800

    Update sudoku.h

--- sudoku/sudoku.h
+++ sudoku/sudoku.h
@@ -3,13 +3,13 @@
 #define _SUDUKU_H
 void createshifttable();
 void showsudoku(char sudoku[9][9]);
-void readsudoku(string path, char sudoku[][9][9], int& n);
-void savesudoku(string path, char sudoku[9][9], int access = ios::app);
-void getsudokuFinality2(string path, int number);
-void getsudokuFinality(string path, int number);
+void readsudoku(std::string path, char sudoku[][9][9], int& n);
+void savesudoku(std::string path, char sudoku[9][9], int access = std::ios::app);
+void getsudokuFinality2(std::string path, int number);
+void getsudokuFinality(std::string path, int number);
 void findvalid(char sudoku[][9], int row, int col, bool valid[10]);
 bool checksudoku(char sudoku[][9]);
 bool dosolve(char sudoku[][9]);
-void solvesudoku(string path, string outpath);
+void solvesudoku(std::string path, std::string outpath);
 
 #endif // !_SUDUKU_H
