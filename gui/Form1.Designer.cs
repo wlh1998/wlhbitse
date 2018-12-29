493d942e32d4aa436c17eb498c270026135fd0fe
wlh1998 <34409620+wlh1998@users.noreply.github.com>
2018-12-29 20:34:11 +0800

    Add files via upload
    数独的gui部分

---
+++ gui/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace sudokugui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(197, 195);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 21;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 21;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 21;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 21;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Column5";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 21;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Column6";
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 21;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Column7";
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 21;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Column8";
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 21;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Column9";
+            this.Column9.Name = "Column9";
+            this.Column9.Width = 21;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(243, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 87);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(243, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 87);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "new game";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(30, 250);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 33);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "mesagge";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 311);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ gui/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace sudokugui
+{
+    
+    public partial class Form1 : Form
+    {
+        private char[,] data;
+        public bool Sudokucheck()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    data[i, j] = (char)dataGridView1.Rows[i].Cells[j].Value.ToString().ToCharArray()[0];
+                }
+            }
+            int rowsum, colsum;
+            for (int i = 0; i < 9; i++)
+            {
+                rowsum = 0;
+                colsum = 0;
+                for (int j = 0; j < 9; j++)
+                {
+                    if (data[i, j] == '0')
+                    {
+                        return false;
+                    }
+                    rowsum += data[i, j]-'0';
+                    colsum += data[j, i]-'0';
+                }
+                if(rowsum != 45|| colsum != 45)
+                {
+                    return false;
+                }
+            }
+                int starti = 0;
+                int startj = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                starti = (i*3) % 9;
+                startj = (i/3)*3;
+                int cellsum = 0;
+
+                for (int j = 0; j < 3; j++)
+                {
+                    for(int k = 0; k < 3; k++)
+                    {
+                        cellsum += data[starti+j,startj+k]-'0';
+                    }
+                }
+                if (cellsum != 45)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        public void Newsudoku()
+        {
+            string srank = "123456789";
+            char[] rank = srank.ToCharArray();
+            string sshift = "036147258";
+            char[] shift = sshift.ToCharArray();
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    data[i, j] = rank[(i+shift[j])%9];
+                    
+                }
+            }
+        }
+        public void Newsudokuproblem()
+        {
+            Newsudoku();
+            //mask true 保留
+            bool[,] mask = new bool[9,9];
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+
+                    if ((i + j) % 3 == 0)
+                    {
+                        mask[i, j] = false;
+                    }
+                    else
+                    {
+                        mask[i, j] = true;
+                    }
+                }
+            }
+            Random ran = new Random();
+            int blanknum = ran.Next(31-27, 59-27);
+            int randi = ran.Next(0, 9);
+            int randj = ran.Next(0, 9);
+
+            for (int i = 0; i < blanknum; i++)
+            {
+                while(mask[randi,randj] == false)
+                {
+                    randi = ran.Next(0, 9);
+                    randj = ran.Next(0, 9);
+                }
+                mask[randi, randj] = false;
+            }
+            for (int i =0; i < 9; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    if(mask[i,j] == false)
+                    {
+                        data[i, j] = '0';
+                    }
+                }
+            }
+        } 
+        public Form1()
+        {
+            InitializeComponent();
+            data = new char[9, 9];
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.ColumnHeadersVisible = false;
+            this.Text = "sudoku game";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            Newsudokuproblem();
+
+            for (int i = 0; i < 9; i++)
+            {
+                dataGridView1.Rows.Add();
+                for (int j = 0; j < 9; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = data[i, j];
+                    if(data[i,j] == '0')
+                    {
+                        dataGridView1.Rows[i].Cells[j].ReadOnly = false;
+                        dataGridView1.Rows[i].Cells[j].Style.Font = new Font("Tahoma",10,FontStyle.Bold);
+                    }
+                    else
+                    {
+                        dataGridView1.Rows[i].Cells[j].ReadOnly = true;
+                        dataGridView1.Rows[i].Cells[j].Style.Font = new Font("Tahoma", 10, FontStyle.Regular);
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Sudokucheck())
+            {
+                label1.Text = "结果正确";
+            }
+            else
+            {
+                label1.Text = "结果错误";
+            }
+        }
+    }
+ 
+}
