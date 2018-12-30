6cc9c420ed72109813bb48b2458bdb6b1b6c3f87
wlh1998 <34409620+wlh1998@users.noreply.github.com>
2018-12-30 23:17:22 +0800

    最终版本

--- sudoku/sudoku.h
+++ sudoku/sudoku.h
@@ -11,5 +11,5 @@ void findvalid(char sudoku[][9], int row, int col, bool valid[10]);
 bool checksudoku(char sudoku[][9]);
 bool dosolve(char sudoku[][9]);
 void solvesudoku(std::string path, std::string outpath);
-
+int commandprocess(int argc, char ** argv);
 #endif // !_SUDUKU_H
